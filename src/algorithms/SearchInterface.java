3a5fdbfdc7e84dfeb85dba3a4170aa28aded2b9a
Aya-Abouzeid <eng-aya.abouzaid1419@alexu.edu.eg>
2018-10-18 10:14:34 +0200

    Updating Interface

---
+++ src/algorithms/SearchInterface.java
@@ -0,0 +1,39 @@
+package algorithms;
+
+import java.util.ArrayList;
+
+public interface SearchInterface {
+	
+	/**
+	 * Runs the Algorithm.
+	 */
+	public void search();
+
+	/**
+	 * @return ArrayList of strings representing the path walked to reach the goal
+	 * where the string could only be one of the following values:
+	 * UP , DOWN , LEFT , RIGHT.
+	 */
+	public ArrayList<String> pathToGoal();
+	
+	/**
+	 * 
+	 * @return cost of path walked to reach goal.
+	 */
+	public int pathCost();
+	
+	/**
+	 * @return number of expanded nodes.
+	 */
+	public int nodesExpanded();
+	
+	/**
+	 * @return maximum depth of tree traversal needed to reach goal.
+	 */
+	public int searchDepth();
+	
+	/**
+	 * @return running time of Algorithm in milliseconds.
+	 */
+	public long runningTime();
+}
