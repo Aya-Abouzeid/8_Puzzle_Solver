fdaa4b15c562e74d64393e8eda36e1023fa416b7
Amira Haiba <maro.haiba@gmail.com>
2018-10-18 23:52:13 -0700

    Added AstarState and Astar class for the algorithm.

--- src/algorithms/State.java
+++ src/algorithms/State.java
@@ -14,6 +14,13 @@ public class State {
 		this.parent = parent;
 		
 	}
+
+	public State getParent() {
+		return parent;
+	}
+	public void setParent(State parent) {
+		this.parent = parent;
+	}
 	public int[][] getGame() {
 		return game;
 	}
@@ -31,5 +38,78 @@ public class State {
 	}
 	public void setNeighbours(ArrayList<State> neighbours) {
 		this.neighbours = neighbours;
+	}
+	private void swap (int[][]arr, int x1, int y1, int x2, int y2){
+		int temp = arr[x1][y1];
+		arr[x1][y1] = arr[x2][y2];
+		arr[x2][y2] = temp;
+	}
+	public ArrayList<State> allPossibleNeighbours(){
+		
+		ArrayList<State> neigh = new ArrayList<>();
+		if(zeroPosition.x+1 < 3){
+		System.out.println("The right X move: ");
+		int[][] n = new int[game.length][];
+		for (int i = 0; i < n.length; ++i) {
+	         n[i] = new int[game[i].length];
+	         for (int j = 0; j < n[i].length; ++j) {
+	        	 
+	            n[i][j] = game[i][j];
+	         }
+	      }
+		
+		swap (n,zeroPosition.x,zeroPosition.y,zeroPosition.x+1,zeroPosition.y);
+		
+		System.out.println("swapping : "+n[zeroPosition.x][zeroPosition.y]+" "+ game[zeroPosition.x][zeroPosition.y]);
+		neigh.add(new State(n, new Point(zeroPosition.x+1,zeroPosition.y),this));
+	}
+	
+	if(zeroPosition.x-1 >= 0){
+		
+		int[][] n = new int[game.length][];
+		for (int i = 0; i < n.length; ++i) {
+	         n[i] = new int[game[i].length];
+	         for (int j = 0; j < n[i].length; ++j) {
+	        	 
+	            n[i][j] = game[i][j];
+	         }
+	      }
+		swap (n,zeroPosition.x,zeroPosition.y,zeroPosition.x-1,zeroPosition.y);
+		System.out.println("swapping : "+n[zeroPosition.x][zeroPosition.y]+" "+ game[zeroPosition.x][zeroPosition.y]);
+		neigh.add(new State(n, new Point(zeroPosition.x-1,zeroPosition.y),this));
+	
+	}
+	
+	if(zeroPosition.y+1 < 3){
+		int[][] n = new int[game.length][];
+		for (int i = 0; i < n.length; ++i) {
+	         n[i] = new int[game[i].length];
+	         for (int j = 0; j < n[i].length; ++j) {
+	        	 
+	            n[i][j] = game[i][j];
+	         }
+	      }
+		swap (n,zeroPosition.x,zeroPosition.y,zeroPosition.x,zeroPosition.y+1);
+		System.out.println("swapping : "+n[zeroPosition.x][zeroPosition.y]+" "+ game[zeroPosition.x][zeroPosition.y]);
+		neigh.add(new State(n, new Point(zeroPosition.x,zeroPosition.y+1),this));
+
+	}
+	
+	if(zeroPosition.y-1 >= 0){
+		int[][] n = new int[game.length][];
+		for (int i = 0; i < n.length; ++i) {
+	         n[i] = new int[game[i].length];
+	         for (int j = 0; j < n[i].length; ++j) {
+	        	 
+	            n[i][j] = game[i][j];
+	         }
+	      }
+		swap (n,zeroPosition.x,zeroPosition.y,zeroPosition.x,zeroPosition.y-1);
+		System.out.println("swapping : "+n[zeroPosition.x][zeroPosition.y]+" "+ game[zeroPosition.x][zeroPosition.y]);
+		neigh.add(new State(n, new Point(zeroPosition.x,zeroPosition.y-1),this));
+
+
+	}
+		return neigh;
 	}
 }
