b7c69ee9ae0d637bc9c3675e1aaf45fd0d6fb244
Aya-Abouzeid <eng-aya.abouzaid1419@alexu.edu.eg>
2018-10-19 12:29:30 +0200

    fix get neighbours

--- src/algorithms/State.java
+++ src/algorithms/State.java
@@ -4,36 +4,122 @@ import java.awt.Point;
 import java.util.ArrayList;
 
 public class State {
-	private int game [][] = new int [3][3];
+	private int game[][] = new int[3][3];
 	private Point zeroPosition;
-	private ArrayList <State> neighbours = new ArrayList <>();
+	private ArrayList<State> neighbours = new ArrayList<>();
 	private State parent;
-	public State (int [][] game , Point zeroPosition, State parent){
+
+	public State(int[][] game, Point zeroPosition, State parent) {
 		this.game = game;
 		this.zeroPosition = zeroPosition;
 		this.parent = parent;
-		
+
 	}
+
+	public State getParent() {
+		return parent;
+	}
+
+	public void setParent(State parent) {
+		this.parent = parent;
+	}
+
 	protected int[][] getGame() {
 		return game;
 	}
+
 	protected void setGame(int[][] game) {
 		this.game = game;
 	}
+
 	protected Point getZeroPosition() {
 		return zeroPosition;
 	}
+
 	protected void setZeroPosition(Point zeroPosition) {
 		this.zeroPosition = zeroPosition;
 	}
+
 	protected ArrayList<State> getNeighbours() {
-		return neighbours;
+		return allPossibleNeighbours();
 	}
+
 	protected void setNeighbours(ArrayList<State> neighbours) {
 		this.neighbours = neighbours;
 	}
-	
-	protected boolean areEqual( int[][] arr2) {
+
+	private void swap(int[][] arr, int x1, int y1, int x2, int y2) {
+		int temp = arr[x1][y1];
+		arr[x1][y1] = arr[x2][y2];
+		arr[x2][y2] = temp;
+	}
+
+	public ArrayList<State> allPossibleNeighbours() {
+
+		ArrayList<State> neigh = new ArrayList<>();
+		if (zeroPosition.x + 1 < 3 && !( this.parent != null && zeroPosition.x + 1 == this.parent.zeroPosition.x)) {
+			int[][] n = new int[this.game.length][];
+			for (int i = 0; i < n.length; ++i) {
+				n[i] = new int[this.game[i].length];
+				for (int j = 0; j < n[i].length; ++j) {
+
+					n[i][j] = this.game[i][j];
+				}
+			}
+
+			swap(n, zeroPosition.x, zeroPosition.y, zeroPosition.x + 1, zeroPosition.y);
+
+			neigh.add(new State(n, new Point(zeroPosition.x + 1, zeroPosition.y), this));
+		}
+
+		if (zeroPosition.x - 1 >= 0 && !( this.parent != null && zeroPosition.x - 1 == this.parent.zeroPosition.x)) {
+
+			int[][] n = new int[this.game.length][];
+			for (int i = 0; i < n.length; ++i) {
+				n[i] = new int[this.game[i].length];
+				for (int j = 0; j < n[i].length; ++j) {
+
+					n[i][j] = this.game[i][j];
+				}
+			}
+			swap(n, zeroPosition.x, zeroPosition.y, zeroPosition.x - 1, zeroPosition.y);
+			neigh.add(new State(n, new Point(zeroPosition.x - 1, zeroPosition.y), this));
+
+		}
+
+		if (zeroPosition.y + 1 < 3 &&  !( this.parent != null && zeroPosition.y + 1 == this.parent.zeroPosition.y )) {
+			int[][] n = new int[this.game.length][];
+			for (int i = 0; i < n.length; ++i) {
+				n[i] = new int[this.game[i].length];
+				for (int j = 0; j < n[i].length; ++j) {
+
+					n[i][j] = this.game[i][j];
+				}
+			}
+			swap(n, zeroPosition.x, zeroPosition.y, zeroPosition.x, zeroPosition.y + 1);
+			neigh.add(new State(n, new Point(zeroPosition.x, zeroPosition.y + 1), this));
+
+		}
+
+		if (zeroPosition.y - 1 >= 0 &&  !( this.parent != null && zeroPosition.y - 1 == this.parent.zeroPosition.y )) {
+			int[][] n = new int[this.game.length][];
+			for (int i = 0; i < n.length; ++i) {
+				n[i] = new int[this.game[i].length];
+				for (int j = 0; j < n[i].length; ++j) {
+
+					n[i][j] = this.game[i][j];
+				}
+			}
+			swap(n, zeroPosition.x, zeroPosition.y, zeroPosition.x, zeroPosition.y - 1);
+			neigh.add(new State(n, new Point(zeroPosition.x, zeroPosition.y - 1), this));
+
+		}
+
+		this.neighbours = neigh;
+		return neigh;
+	}
+
+	protected boolean areEqual(int[][] arr2) {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				if (this.game[i][j] != arr2[i][j])
@@ -53,5 +139,6 @@ public class State {
 			}
 		}
 		return true;
+
 	}
 }
