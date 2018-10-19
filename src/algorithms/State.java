48c09272b08cd149ff3ba057d9ba54877d2e9cc0
Amira Haiba <maro.haiba@gmail.com>
2018-10-19 00:01:21 -0700

    Saving State class.

--- src/algorithms/State.java
+++ src/algorithms/State.java
@@ -14,7 +14,6 @@ public class State {
 		this.parent = parent;
 		
 	}
-<<<<<<< HEAD
 
 	public State getParent() {
 		return parent;
@@ -22,10 +21,8 @@ public class State {
 	public void setParent(State parent) {
 		this.parent = parent;
 	}
-	public int[][] getGame() {
-=======
+
 	protected int[][] getGame() {
->>>>>>> 51e57401b88899cb7ca5f99f1b5362046c399815
 		return game;
 	}
 	protected void setGame(int[][] game) {
@@ -43,7 +40,7 @@ public class State {
 	protected void setNeighbours(ArrayList<State> neighbours) {
 		this.neighbours = neighbours;
 	}
-<<<<<<< HEAD
+
 	private void swap (int[][]arr, int x1, int y1, int x2, int y2){
 		int temp = arr[x1][y1];
 		arr[x1][y1] = arr[x2][y2];
@@ -54,69 +51,69 @@ public class State {
 		ArrayList<State> neigh = new ArrayList<>();
 		if(zeroPosition.x+1 < 3){
 		System.out.println("The right X move: ");
-		int[][] n = new int[game.length][];
+		int[][] n = new int[this.game.length][];
 		for (int i = 0; i < n.length; ++i) {
-	         n[i] = new int[game[i].length];
+	         n[i] = new int[this.game[i].length];
 	         for (int j = 0; j < n[i].length; ++j) {
 	        	 
-	            n[i][j] = game[i][j];
+	            n[i][j] = this.game[i][j];
 	         }
 	      }
 		
 		swap (n,zeroPosition.x,zeroPosition.y,zeroPosition.x+1,zeroPosition.y);
 		
-		System.out.println("swapping : "+n[zeroPosition.x][zeroPosition.y]+" "+ game[zeroPosition.x][zeroPosition.y]);
+		System.out.println("swapping : "+n[zeroPosition.x][zeroPosition.y]+" "+ this.game[zeroPosition.x][zeroPosition.y]);
 		neigh.add(new State(n, new Point(zeroPosition.x+1,zeroPosition.y),this));
 	}
 	
 	if(zeroPosition.x-1 >= 0){
 		
-		int[][] n = new int[game.length][];
+		int[][] n = new int[this.game.length][];
 		for (int i = 0; i < n.length; ++i) {
-	         n[i] = new int[game[i].length];
+	         n[i] = new int[this.game[i].length];
 	         for (int j = 0; j < n[i].length; ++j) {
 	        	 
-	            n[i][j] = game[i][j];
+	            n[i][j] = this.game[i][j];
 	         }
 	      }
 		swap (n,zeroPosition.x,zeroPosition.y,zeroPosition.x-1,zeroPosition.y);
-		System.out.println("swapping : "+n[zeroPosition.x][zeroPosition.y]+" "+ game[zeroPosition.x][zeroPosition.y]);
+		System.out.println("swapping : "+n[zeroPosition.x][zeroPosition.y]+" "+ this.game[zeroPosition.x][zeroPosition.y]);
 		neigh.add(new State(n, new Point(zeroPosition.x-1,zeroPosition.y),this));
 	
 	}
 	
 	if(zeroPosition.y+1 < 3){
-		int[][] n = new int[game.length][];
+		int[][] n = new int[this.game.length][];
 		for (int i = 0; i < n.length; ++i) {
-	         n[i] = new int[game[i].length];
+	         n[i] = new int[this.game[i].length];
 	         for (int j = 0; j < n[i].length; ++j) {
 	        	 
-	            n[i][j] = game[i][j];
+	            n[i][j] = this.game[i][j];
 	         }
 	      }
 		swap (n,zeroPosition.x,zeroPosition.y,zeroPosition.x,zeroPosition.y+1);
-		System.out.println("swapping : "+n[zeroPosition.x][zeroPosition.y]+" "+ game[zeroPosition.x][zeroPosition.y]);
+		System.out.println("swapping : "+n[zeroPosition.x][zeroPosition.y]+" "+ this.game[zeroPosition.x][zeroPosition.y]);
 		neigh.add(new State(n, new Point(zeroPosition.x,zeroPosition.y+1),this));
 
 	}
 	
 	if(zeroPosition.y-1 >= 0){
-		int[][] n = new int[game.length][];
+		int[][] n = new int[this.game.length][];
 		for (int i = 0; i < n.length; ++i) {
-	         n[i] = new int[game[i].length];
+	         n[i] = new int[this.game[i].length];
 	         for (int j = 0; j < n[i].length; ++j) {
 	        	 
-	            n[i][j] = game[i][j];
+	            n[i][j] = this.game[i][j];
 	         }
 	      }
 		swap (n,zeroPosition.x,zeroPosition.y,zeroPosition.x,zeroPosition.y-1);
-		System.out.println("swapping : "+n[zeroPosition.x][zeroPosition.y]+" "+ game[zeroPosition.x][zeroPosition.y]);
+		System.out.println("swapping : "+n[zeroPosition.x][zeroPosition.y]+" "+ this.game[zeroPosition.x][zeroPosition.y]);
 		neigh.add(new State(n, new Point(zeroPosition.x,zeroPosition.y-1),this));
 
 
 	}
 		return neigh;
-=======
+	}
 	
 	protected boolean areEqual( int[][] arr2) {
 		for (int i = 0; i < 3; i++) {
@@ -138,6 +135,6 @@ public class State {
 			}
 		}
 		return true;
->>>>>>> 51e57401b88899cb7ca5f99f1b5362046c399815
+
 	}
 }
