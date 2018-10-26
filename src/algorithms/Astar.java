dc4ccbff9f9fa478473c65a77c9085ccb2569f55
Amira Haiba <maro.haiba@gmail.com>
2018-10-26 13:58:56 -0700

    Implemented SearchInterface.

--- src/algorithms/Astar.java
+++ src/algorithms/Astar.java
@@ -7,8 +7,14 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 
-public class Astar {
+public class Astar implements SearchInterface {
 	private HashMap <Integer,Point> goalState = new HashMap<>();	
+	private long elapsedTime;
+	private long startTime;
+	private long stopTime;
+	private AstarState finalState;
+	private ArrayList<AstarState> explored;
+
 	
 	public Astar(){
 		goalState.put(0, new Point(0,0));
@@ -28,7 +34,7 @@ public class Astar {
 		arr[x1][y1] = arr[x2][y2];
 		arr[x2][y2] = temp;
 	}
-	private boolean checkNotInExplored(int[][] arr, ArrayList<AstarState> explored){
+	private boolean checkNotInExplored(int[][] arr){
 		for(AstarState s : explored){
 			if (Arrays.toString(s.getState().getGame()[0]).equals(Arrays.toString(arr[0])) 
 					&& Arrays.toString(s.getState().getGame()[1]).equals(Arrays.toString(arr[1])) 
@@ -41,20 +47,17 @@ public class Astar {
 	}
 	private int computeNewH(int [][] arr){
 		int h = 0 ;
-		boolean finalState = false;
+		boolean finalSt = false;
 		for (int i = 0 ; i < 3 ; i++){
 			for(int j = 0 ; j < 3 ; j++){
 				if(arr[i][j] == 0)
 					continue;
 				Point rightPos = goalState.get(arr[i][j]);
-				//System.out.println("value " + arr[i][j] + " i&j "+  i+" "+j +"  "+ "rights " + rightPos.x + " " +rightPos.y);
 				h+= Math.abs(i-rightPos.x) + Math.abs(j-rightPos.y);
 				if(i==0 && j==0 && h==0)
-					finalState = true;
+					finalSt = true;
 			}
 		}
-		if(finalState && h !=0)
-			return -1;
 		
 		return h;
 	}
@@ -72,7 +75,7 @@ public class Astar {
 		}
 		return true;
 	}
-	public void algorithm (State state) {
+	public boolean astarAlgo(State state) {
 		
 		AstarState initialState = new AstarState(state, 0, 0);
 		PriorityQueue<AstarState> frontier = new PriorityQueue<>(new Comparator <AstarState>() {
@@ -86,41 +89,122 @@ public class Astar {
 		});
 		initialState.setH(computeNewH(initialState.getState().getGame()));
 		frontier.add(initialState);
-		
-		ArrayList<AstarState> explored = new ArrayList<>();
-		
+				
 		while(!frontier.isEmpty()){
 			
 			AstarState s = frontier.poll();
-			System.out.println("frontier: "+Arrays.toString(s.getState().getGame()[0]));
 			explored.add(s);
-			if (s.getH() == 0)
-				break;
+			for(int i = 0 ; i < 3 ; i++){
+				for(int j = 0 ; j <3 ; j++){
+					System.out.print(s.getState().getGame()[i][j]+" ");
+				}
+			}
+			System.out.println("  ");
+			if (s.getH() == 0){
+				stopTime = System.currentTimeMillis();
+				elapsedTime = stopTime - startTime;
+				finalState = s;
+				return true;
+			}
 			ArrayList<State> posN = s.getState().allPossibleNeighbours();
 
 			for(State st : posN){
-				boolean notExplored = checkNotInExplored(st.getGame(),explored);
-				System.out.println("Boolean not Explored4  : "+notExplored);
+				boolean notExplored = checkNotInExplored(st.getGame());
 
 				int h;
 				if (notExplored){
 					h= computeNewH(st.getGame());
-					System.out.println("value of h  "+h);
-
-					
-					if(h != -1){
 						boolean notInFrontier = DecreaseKeyIfInFrontierAndNessesary(st.getGame(),s.getG()+1+h,frontier);
 						if(notInFrontier){
 							
 							s.getState().getNeighbours().add(st);
 							AstarState newAstarState = new AstarState(st, s.getG()+1, h);
 							frontier.add(newAstarState);
-		
-						}
 					}
 				}
 				
 			}
-		}		
+		}	
+		
+		return false;
+	}
+	@Override
+	public boolean search(int[] initialState) {
+		// TODO Auto-generated method stub
+		startTime = System.currentTimeMillis();
+		explored = new ArrayList<AstarState>();
+		if (initialState == null || initialState.length != 9) {
+			stopTime = System.currentTimeMillis();
+			elapsedTime = stopTime - startTime;
+			return false;
+		}
+
+		Point zeroIndex = new Point(-1, -1);
+
+		int[][] state = new int[3][3];
+		for (int i = 0; i < 3; i++) {
+
+			for (int j = 0; j < 3; j++) {
+
+				if (initialState[i * 3 + j] == 0) {
+					// check if there are multiple zeros
+					if (zeroIndex.x == -1)
+						zeroIndex = new Point(i, j);
+					else {
+						stopTime = System.currentTimeMillis();
+						elapsedTime = stopTime - startTime;
+						return false;
+					}
+				}
+				state[i][j] = initialState[i * 3 + j];
+			}
+		}
+		// check if no zeros found
+		if (zeroIndex.x == -1) {
+			stopTime = System.currentTimeMillis();
+			elapsedTime = stopTime - startTime;
+			return false;
+		}
+
+		State inital = new State(state, zeroIndex, null, null);
+		return astarAlgo(inital);
+	}
+	@Override
+	public ArrayList<String> pathToGoal() {
+		ArrayList<State> parents = new ArrayList<State>();
+		ArrayList<String> path = new ArrayList<String>();
+		State s = finalState.getState();
+		while (s != null) {
+			parents.add(s);
+			s = s.getParent();
+		}
+		for (int i = parents.size() - 2; i >= 0; i--) {
+			path.add(parents.get(i).getPath());
+		}
+		return path;
+	}
+	@Override
+	public int pathCost() {
+		return finalState.getCost();
+	}
+	@Override
+	public int nodesExpanded() {
+		return explored.size();
+	}
+	@Override
+	public int searchDepth() {
+		int depth = -1;
+		State s = finalState.getState();
+		while (s != null) {
+			depth++;
+			s = s.getParent();
+		}
+
+		return depth;
+	}
+	@Override
+	public long runningTime() {
+		// TODO Auto-generated method stub
+		return elapsedTime;
 	}
 }
